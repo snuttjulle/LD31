285b743be8ad30bf1fb96407ee38d29a953266ab
Snutt <anders@art-studio.se>
2014-12-06 23:29:53 +0100

    Implemented touch behavior

---
+++ Assets/Button.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Button : MonoBehaviour
+{
+	Action<object> OnTriggerCallback;
+	BoxCollider2D _collider;
+
+	void Awake()
+	{
+		_collider = GetComponent<BoxCollider2D>();
+	}
+
+	void Update()
+	{
+		for (var i = 0; i < Input.touchCount; i++)
+		{
+			if (Input.GetTouch(i).phase == TouchPhase.Began)
+			{
+				RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position), Vector2.zero);
+				if (hitInfo)
+				{
+					OnPress(hitInfo);
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
+			if (hitInfo)
+			{
+				OnPress(hitInfo);
+			}
+		}
+	}
+
+	void OnPress(RaycastHit2D hitInfo)
+	{
+		if (_collider.bounds.Contains(new Vector3(hitInfo.point.x, hitInfo.point.y, _collider.transform.position.z)))
+		{
+			//Debug.Log(hitInfo.transform.gameObject.name);
+
+			if (OnTriggerCallback != null)
+				OnTriggerCallback(this);
+		}
+	}
+
+	public void SetTriggerCallback(Action<object> callback)
+	{
+		OnTriggerCallback = callback;
+	}
+}
---
+++ Assets/scripts/NoticeHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoticeHandler : MonoBehaviour
+{
+	public Animator NoticeGraphics;
+
+	void Start()
+	{
+		Button button = GetComponent<Button>();
+		button.SetTriggerCallback(OnPress);
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnPress(object sender)
+	{
+		Debug.Log("Press:" + sender);
+		NoticeGraphics.SetTrigger("remove");
+	}
+}
